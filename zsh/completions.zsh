f0a7656c2259c6a416e1a595599419751dc49b0d
Omar Sabry <ohsabry@gmail.com>
2016-08-12 17:10:16 -0400

    add some color hackery for tab completion

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -3,6 +3,10 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # Fancy prompt_toolkit style selection menu on TAB
 zstyle ':completion:*' menu select
+# Group completion
+#
+highlights='${PREFIX:+=(#bi)($PREFIX:t)(?)*==$color[blue]=$color[blue];$color[cyan]}':${(s.:.)LS_COLORS}}
 
+zstyle -e ':completion:*:default' list-colors 'reply=( "'$highlights'" )'
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
