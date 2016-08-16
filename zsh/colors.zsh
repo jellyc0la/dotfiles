69b97f5fa214d3957dce793b92dcb09b7efb5afd
Omar Sabry <ohsabry@gmail.com>
2016-08-16 16:17:11 -0400

    colorize man pages

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -5,6 +5,18 @@ unset LSCOLORS
 
 export CLICOLOR=1
 export LS_COLORS="exfxcxdxbxegedabagacad"
+
+man() {
+    env \
+        LESS_TERMCAP_md=$'\e[1;31m' \
+        LESS_TERMCAP_me=$'\e[0m' \
+        LESS_TERMCAP_se=$'\e[0m' \
+        LESS_TERMCAP_so=$'\e[1;44;32m' \
+        LESS_TERMCAP_ue=$'\e[0m' \
+        LESS_TERMCAP_us=$'\e[1;33m' \
+            man "$@"
+}
+
 # Base16 Shell
 BASE16_SHELL="$HOME/.dotfiles/zsh/base16-default.dark.sh"
 [[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
