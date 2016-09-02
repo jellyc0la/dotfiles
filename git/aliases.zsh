5086b4a919dacdc7a8d5fac01dd74f42f2ea7583
Omar Sabry <ohsabry@gmail.com>
2016-09-01 19:34:48 -0700

    add more git aliases and config

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,8 +2,10 @@ hub_path=$(which hub)
 alias git=$hub_path
 
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glep="git log -E -i --grep"
 alias gd='git diff'
 alias gb='git branch'
 alias gco='git checkout'
 alias gc='git commit'
 alias gs='git status -b'
+alias gca='git commit --amend --no-edit'
