cd4b07882bdfddef8727d320d3d14e2af8e46400
Omar Sabry <ohsabry@gmail.com>
2017-09-01 10:18:56 -0700

    use pyenv

--- python/python.zsh
+++ python/python.zsh
@@ -1,4 +1,7 @@
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
 # Serve the current directory. Pass in a port, or use default 8000
 function simple-server() {
